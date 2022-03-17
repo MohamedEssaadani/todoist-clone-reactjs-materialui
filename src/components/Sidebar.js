218cbed82e2e54a9d5c199f9624677f3b180f93c
Mohamed Es-saadani <essaadani80@gmail.com>
2022-03-17 16:56:00 +0100

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -20,6 +20,15 @@ function Sidebar() {
         {menuItem(CalendarTodayOutlinedIcon, "Aujourd'hui", "#048527")}
         {menuItem(CalendarMonthOutlinedIcon, "Prochainement", "#692FC2")}
         {menuItem(AppsOutlinedIcon, "Filtres et étiquettes", "#EA8915")}
+
+        <div className="sidebar__projectsMenu">
+          <div className="sidebar__projectsMenuProject">
+            <p>Projets</p>
+          </div>
+          <div className="sidebar__projectsMenuArchived">
+            <p>Projets Archivés</p>
+          </div>
+        </div>
       </div>
     </div>
   );
